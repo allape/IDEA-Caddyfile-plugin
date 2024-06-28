44724b5b7dbba8f11c1bba1fafbc32e9915e9665
as <allensnape@qq.com>
2024-06-28 19:54:04 +0800

    fix: no error in renaming now, but still, not working

---
+++ src/main/kotlin/cc/allape/caddyfile/LanguageNamesValidator.kt
@@ -0,0 +1,16 @@
+package cc.allape.caddyfile
+
+import com.intellij.lang.refactoring.NamesValidator
+import com.intellij.openapi.project.Project
+
+class CaddyfileLanguageNamesValidator : NamesValidator {
+
+    override fun isKeyword(name: String, project: Project): Boolean {
+        return false
+    }
+
+    override fun isIdentifier(name: String, project: Project): Boolean {
+        return name.startsWith("@")
+    }
+
+}
