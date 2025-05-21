d6649b32af7f579db586483c4bbb6db4a66480a7
as <allensnape@gmail.com>
2025-05-21 16:49:55 +0800

    fix: header completion

--- src/main/kotlin/cc/allape/caddyfile/CompletionContributor.kt
+++ src/main/kotlin/cc/allape/caddyfile/CompletionContributor.kt
@@ -5,11 +5,49 @@ import cc.allape.caddyfile.language.psi.*
 import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.patterns.PlatformPatterns
+import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.util.elementType
 import com.intellij.util.ProcessingContext
 
 internal class CaddyfileCompletionContributor : CompletionContributor() {
+    fun argCompletion(argEle: PsiElement, resultSet: CompletionResultSet) {
+        // Broken too bad
+//        val prevArg = argEle.prevSibling
+//
+//        if (prevArg != null) {
+//            val text = prevArg.text
+//
+//            // fill MIME
+//            if (MIME_HEADERS.contains(text)) {
+//                COMMON_MIME.forEach { mime -> resultSet.addElement(LookupElementBuilder.create(mime)) }
+//                return
+//            }
+//
+//            // try_files.policy
+//            // https://caddyserver.com/docs/caddyfile/directives/try_files#syntax
+//            if (text == "policy") {
+//                resultSet.addElement(LookupElementBuilder.create("first_exist"))
+//                resultSet.addElement(LookupElementBuilder.create("smallest_size"))
+//                resultSet.addElement(LookupElementBuilder.create("largest_size"))
+//                resultSet.addElement(LookupElementBuilder.create("most_recently_modified"))
+//                return
+//            }
+//        }
+
+        val dir = argEle.parent?.firstChild
+
+        // http header
+        if (dir?.text?.contains("header") == true) {
+            HTTP_HEADERS.forEach {
+                resultSet.addElement(
+                    LookupElementBuilder.create("${it.key} ").withLookupString(it.key.lowercase())
+                )
+            }
+            return
+        }
+    }
+
     init {
         // directive completion
         extend(
@@ -17,7 +55,7 @@ internal class CaddyfileCompletionContributor : CompletionContributor() {
             PlatformPatterns.psiElement(CaddyfileTypes.DIRECTIVE),
             object : CompletionProvider<CompletionParameters?>() {
                 override fun addCompletions(
-                    parameters: CompletionParameters, context: ProcessingContext, resultSet: CompletionResultSet
+                    parameters: CompletionParameters, context: ProcessingContext, resultSet: CompletionResultSet,
                 ) {
                     val ele = parameters.originalFile.findElementAt(parameters.offset - 1) ?: return
                     val parentDir = ele.parent?.parent?.parent?.firstChild
@@ -48,7 +86,7 @@ internal class CaddyfileCompletionContributor : CompletionContributor() {
             PlatformPatterns.psiElement(CaddyfileTypes.SNIPPET_NAME_TEXT),
             object : CompletionProvider<CompletionParameters?>() {
                 override fun addCompletions(
-                    parameters: CompletionParameters, context: ProcessingContext, resultSet: CompletionResultSet
+                    parameters: CompletionParameters, context: ProcessingContext, resultSet: CompletionResultSet,
                 ) {
                     PsiTreeUtil.findChildrenOfType(parameters.originalFile, CaddyfileSnippetDeclaration::class.java)
                         .forEach { snippet ->
@@ -67,7 +105,7 @@ internal class CaddyfileCompletionContributor : CompletionContributor() {
             ),
             object : CompletionProvider<CompletionParameters?>() {
                 override fun addCompletions(
-                    parameters: CompletionParameters, context: ProcessingContext, resultSet: CompletionResultSet
+                    parameters: CompletionParameters, context: ProcessingContext, resultSet: CompletionResultSet,
                 ) {
                     PsiTreeUtil.findChildrenOfType(parameters.originalFile, CaddyfileMatcherDeclaration::class.java)
                         .forEach { md ->
@@ -85,55 +123,26 @@ internal class CaddyfileCompletionContributor : CompletionContributor() {
             PlatformPatterns.psiElement(CaddyfileTypes.VARIABLE_NAME),
             object : CompletionProvider<CompletionParameters?>() {
                 override fun addCompletions(
-                    parameters: CompletionParameters, context: ProcessingContext, resultSet: CompletionResultSet
+                    parameters: CompletionParameters, context: ProcessingContext, resultSet: CompletionResultSet,
                 ) {
                     PLACEHOLDERS.forEach { resultSet.addElement(LookupElementBuilder.create(it)) }
                 }
             }
         )
 
-        // arg completion
         extend(
             CompletionType.BASIC,
-            PlatformPatterns.psiElement(CaddyfileTypes.ARG),
+            PlatformPatterns.psiElement(CaddyfileTypes.TEXT),
             object : CompletionProvider<CompletionParameters?>() {
                 override fun addCompletions(
-                    parameters: CompletionParameters, context: ProcessingContext, resultSet: CompletionResultSet
+                    parameters: CompletionParameters, context: ProcessingContext, resultSet: CompletionResultSet,
                 ) {
                     val ele = parameters.originalFile.findElementAt(parameters.offset - 1) ?: return
 
-                    val prevArg = ele.prevSibling?.prevSibling
-
-                    if (prevArg != null) {
-                        val text = prevArg.text
-
-                        // fill MIME
-                        if (MIME_HEADERS.contains(text)) {
-                            COMMON_MIME.forEach { mime -> resultSet.addElement(LookupElementBuilder.create(mime)) }
-                            return
-                        }
-
-                        // try_files.policy
-                        // https://caddyserver.com/docs/caddyfile/directives/try_files#syntax
-                        if (text == "policy") {
-                            resultSet.addElement(LookupElementBuilder.create("first_exist"))
-                            resultSet.addElement(LookupElementBuilder.create("smallest_size"))
-                            resultSet.addElement(LookupElementBuilder.create("largest_size"))
-                            resultSet.addElement(LookupElementBuilder.create("most_recently_modified"))
-                            return
-                        }
-                    }
-
-                    val dir = ele.parent?.firstChild
-
-                    // http header
-                    if (dir?.text?.contains("header") == true) {
-                        HTTP_HEADERS.forEach {
-                            resultSet.addElement(
-                                LookupElementBuilder.create("${it.key} ").withLookupString(it.key.lowercase())
-                            )
-                        }
-                        return
+                    // arg completion
+                    val argEle = ele.parent?.parent?.parent
+                    if (argEle is CaddyfileArg) {
+                        argCompletion(argEle, resultSet)
                     }
                 }
             }
